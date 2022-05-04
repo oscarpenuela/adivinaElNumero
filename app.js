9ea79b5c670ca52c8f403ece2a2f00dcec422adf
Oscar Penuela <oscar_apenuela@hotmail.com>
2022-05-04 14:35:49 -0500

    Aplicacion basica JS

---
+++ app.js
@@ -0,0 +1,56 @@
+window.addEventListener("load", function(){
+    let numeroAleatorio = Math.floor(Math.random()*100)+1;
+    const numero = document.querySelector('.numero');
+    const intentoEnviado = document.querySelector('.intentoEnviado');
+    const resultado = document.querySelector('.resultado');
+    const pista = document.querySelector('.pista');
+    let intentos = 1;    
+    let reiniciarBoton;
+    
+    function reiniciarJuego(){
+        reiniciarBoton = document.createElement("button");
+        reiniciarBoton.textContent = "Reiniciar Juego";
+        document.querySelector('.game').appendChild(reiniciarBoton);        
+        reiniciarBoton.addEventListener('click', function(){
+            resultado.textContent = '';
+            pista.textContent = '';
+            numero.value = '';
+            pista.style.backgroundColor = 'white';
+            document.querySelector('.game').removeChild(reiniciarBoton);
+            numeroAleatorio = Math.floor(Math.random()*100)+1;
+            numero.focus();
+            intentos = 1;
+        });
+    }
+    
+    function validarNumero(n){        
+        if(n === numeroAleatorio){            
+            pista.textContent  = "FELICITACIONES: Lo adivinaste!!!";
+            pista.style.backgroundColor = "green";       
+            reiniciarJuego();
+        }else if (intentos === 10){
+            pista.textContent  = "FALLASTE!!";
+            reiniciarJuego();
+        }else if(n< numeroAleatorio){            
+            pista.textContent  = "Número muy bajo!";
+            pista.style.backgroundColor = "red";            
+        } else {
+            pista.textContent  = "Número muy alto";            
+            pista.style.backgroundColor = "red";            
+        }        
+        intentos++;
+        numero.value = "";
+        numero.focus();
+    }
+    numero.focus();
+    intentoEnviado.addEventListener('click', function(){
+        let numeroDigitado = Number(numero.value);
+        if(intentos === 1){
+            resultado.textContent = 'Intentos anteriores: ';
+        }
+        validarNumero(numeroDigitado);
+        resultado.textContent += " "+numeroDigitado;
+    })
+})
+
+
